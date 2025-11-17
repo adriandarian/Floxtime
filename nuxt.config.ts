c4908241d8e65c9f1d60791c9994d707ce5c0365
Adrian Darian <adrian.the.hactus@gmail.com>
2025-11-16 17:15:23 -0800

    Add Nuxt UI library for better component support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt'],
+  modules: ['@vueuse/nuxt', '@nuxt/ui'],
   runtimeConfig: {
     mongodbUri: process.env.MONGODB_URI || '',
     public: {}
